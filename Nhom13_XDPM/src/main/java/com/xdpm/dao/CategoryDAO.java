b644995ddae2f906a9475bc648bb541d50695285
thanhkhoapham <thanhkhoapham01@gmail.com>
2021-08-08 19:16:16 +0700

    Xong 9 UI Khac- Gia/ngay thue Category,CategoryDao, 2 cái UI đó Thay doi: Thêm 2 constructor có id,name,ngày thêm 					id,name,giá thuê, phí trễ

--- Nhom13_XDPM/src/main/java/com/xdpm/dao/CategoryDAO.java
+++ Nhom13_XDPM/src/main/java/com/xdpm/dao/CategoryDAO.java
@@ -6,15 +6,29 @@ import javax.persistence.Query;
 
 import com.xdpm.entity.Category;
 
-public class CategoryDAO extends AbstractCRUD<Category>{
-	
+public class CategoryDAO extends AbstractCRUD<Category> {
+
+	@SuppressWarnings("unchecked")
 	public List<Category> getAllCategory() {
 		Query query = entityManager.createQuery("from Category", Category.class);
 		return query.getResultList();
 	}
-	
+
 	public Category getCategoryByID(int id) {
 		return entityManager.find(Category.class, id);
 	}
-	
+
+	public Category getCategoryByName(String name) {
+		Query query = entityManager.createQuery("from Category where name='" + name + "'", Category.class);
+		return (Category) query.getSingleResult();
+	}
+
+	@Override
+	public Category update(Category t) {
+		Category category = entityManager.find(Category.class, t.getId());
+		category.setLateFee(t.getLateFee());
+		category.setRentalCharge(t.getRentalCharge());
+		category.setRentalPeriod(t.getRentalPeriod());
+		return super.update(category);
+	}
 }
